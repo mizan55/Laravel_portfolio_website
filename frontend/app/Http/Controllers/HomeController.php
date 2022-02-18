d927cebe5454ec6f4a5021e99fa08688bcf46649
Mohammad Mizanur Rashed <39917200+mizan55@users.noreply.github.com>
2022-02-18 15:03:49 +0600

    Service Frontend
    Service Frontend

--- frontend/app/Http/Controllers/HomeController.php
+++ frontend/app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\VisitorModel;
+use App\Models\ServicesModel;
 
 use Illuminate\Http\Request;
 
@@ -13,6 +14,10 @@ class HomeController extends Controller
         date_default_timezone_set('Asia/Dhaka');// timezone set
         $timeDate = date(format: "Y-m-d h:i:sa"); //to catch when visit
         VisitorModel::insert(['ip_address'=> $UserIp,'visit_time'=>$timeDate ]);
-        return view('Home');
+
+        //service data fetch
+        $ServiceData=json_decode(ServicesModel::all());
+        //end service data fetch
+        return view('Home',['ServiceData'=>$ServiceData]);
     }
 }
--- frontend/resources/views/home.blade.php
+++ frontend/resources/views/home.blade.php
@@ -1,1 +1,7 @@
 @extends('layout.app');
+
+@section('content');
+@include('Component.HomeBanner');
+@include('Component.HomeService');
+
+@endsection
